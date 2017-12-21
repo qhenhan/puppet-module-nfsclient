cc237351cf733201ae6217bb89525f186f3c6f36
Anders Bostrom <anders.l.bostrom@ericsson.com>
2017-12-21 14:08:46 +0100

    Added Ubuntu support

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,10 +16,12 @@ class nfsclient (
 
   case $::osfamily {
     'RedHat': {
+      include nfs::idmap
       $gss_line     = 'SECURE_NFS'
       $keytab_line  = 'RPCGSSDARGS'
       $service      = 'rpcgssd'
       $nfs_requires = Service['idmapd_service']
+      $nfs_sysconf  = '/etc/sysconfig/nfs'
       if $::operatingsystemrelease =~ /^7/ {
         if $keytab {
           service { 'nfs-config':
@@ -29,7 +31,7 @@ class nfsclient (
           }
           file { '/etc/krb5.keytab':
             ensure => 'symlink',
-            target => '/etc/opt/quest/vas/host.keytab',
+            target => "${keytab}",
           }
           if $gss_real {
             Service['nfs-config'] ~> Service['rpcgssd']
@@ -48,6 +50,7 @@ class nfsclient (
       # Setting nfs_requires to undef just fixes the code
       # Someone with better functional knowledge should fix functionality if needed or remove this comment.
       $nfs_requires = undef
+      $nfs_sysconf  = '/etc/sysconfig/nfs'
       if $::operatingsystemrelease =~ /^11/ {
         if $gss_real {
           file_line { 'NFS_START_SERVICES':
@@ -71,17 +74,40 @@ class nfsclient (
         }
       }
     }
+    'Debian': {
+      $gss_line     = 'NEED_GSSD'
+      $keytab_line  = 'GSSDARGS'
+      $service      = 'gssd'
+      $nfs_requires = undef
+      $nfs_sysconf  = '/etc/default/nfs-common'
+      # Puppet 3.x Incorrectly defaults to upstart for Ubuntu 16.x
+      if $::lsbmajdistrelease == 16 and $::lsbdistid == 'Ubuntu' {
+        Service {
+          provider => 'systemd',
+        }
+      }
+      if $keytab {
+        file { '/etc/krb5.keytab':
+          ensure => 'symlink',
+          target => "${keytab}",
+        }
+        if $gss_real {
+          File['/etc/krb5.keytab'] {
+            notify => Service["$service"],
+          }
+        }
+      }
+    }
     default: {
-      fail("nfsclient module only supports Suse and RedHat. <${::osfamily}> was detected.")
+      fail("nfsclient module only supports Suse, RedHat and Debian. <${::osfamily}> was detected.")
     }
   }
 
   if $gss_real {
     include rpcbind
-    include nfs::idmap
 
     file_line { 'NFS_SECURITY_GSS':
-      path   => '/etc/sysconfig/nfs',
+      path   => "${nfs_sysconf}",
       line   => "${gss_line}=\"yes\"",
       match  => "^${gss_line}=.*",
       notify => Service[rpcbind_service],
@@ -99,7 +125,7 @@ class nfsclient (
   }
   if $keytab {
     file_line { 'GSSD_OPTIONS':
-      path  => '/etc/sysconfig/nfs',
+      path  => "${nfs_sysconf}",
       line  => "${keytab_line}=\"-k ${keytab}\"",
       match => "^${keytab_line}=.*",
     }
